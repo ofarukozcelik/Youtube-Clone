adab5689a9311bb5d13440c02e1eb0f7be961a01
ofarukozcelik <ofaruko06@gmail.com>
2024-06-21 23:30:12 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Youtube Clone
+Youtube Clone projesi geliştirildi.
+
+# Kullanılan Teknolojiler
+- React + Vite
+
+# Ekran Görüntüsü
+![](public/youtube-clone.png)
+
+# GIF
+![](public/youtube-clone.gif)
---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+/*
+ * Yapılan her istekte geçerli olmasını istedğimiz ayarları tanımladığımız bir axios örneği oluşturduk.
+ */
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  params: {
+    geo: "TR",
+    lang: "tr",
+  },
+  headers: {
+    "X-RapidAPI-Key": import.meta.env.VITE_API_KEY,
+    "X-RapidAPI-Host": "yt-api.p.rapidapi.com",
+  },
+});
+export default api;
